1cbb68d318dc9252f49dcd773d263aef3b070f36
souvik ghosh <xandersavy@gmail.com>
2022-10-10 22:06:28 +0530

    Random Cocktail fetcher

---
+++ CockTailPreview.js
@@ -0,0 +1,37 @@
+import { View, Image, StyleSheet, Text } from "react-native";
+import { scale } from "react-native-size-matters";
+
+const CockTailPreview = ({ strDrink, strCategory, strDrinkThumb }) => {
+  console.log("I am updating");
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: strDrinkThumb }} style={styles.imageStyle} />
+      <Text>{strDrink}</Text>
+      <Text>{strCategory}</Text>
+      {/* <Text>
+        Hello I am {firstName} {lastName}. I am from {city}. you can mail me at
+        {email}
+      </Text> */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    alignContent: "center",
+    alignItems: "center",
+    padding: scale(10),
+    margin: scale(10),
+    borderColor: "black",
+    borderRadius: 2,
+  },
+  imageStyle: {
+    padding: 10,
+    margin: 10,
+    width: 200,
+    height: 200,
+  },
+});
+
+export default CockTailPreview;
